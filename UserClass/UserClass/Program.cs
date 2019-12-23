cfe9721dceba7f8ed012c2769aadaab4842bbc15
Usman AMIR <Usman.Amir@@live.com>
2019-12-23 17:46:01 +0500

    add method scaffolding from question

--- UserClass/UserClass/Program.cs
+++ UserClass/UserClass/Program.cs
@@ -8,8 +8,21 @@ namespace UserClass
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            while (true)
+            {
+                var user = new User();
+                Console.WriteLine("please enter the username, or q to exit");
+                var userName = Console.ReadLine();
+                if (userName == "q")
+                {
+                    break;
+                }
+
+                user.Add(userName);
+                Console.WriteLine($"number of addedUser {user.GetUsersCount()}");
+            }
         }
     }
 }
