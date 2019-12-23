5db64e14f63fd76278ec7f4f9dd043af09e06b24
Usman AMIR <Usman.Amir@@live.com>
2019-12-23 18:09:04 +0500

    Initialize scaffolding method

--- JohnTheRobot/JohnTheRobot/Program.cs
+++ JohnTheRobot/JohnTheRobot/Program.cs
@@ -8,8 +8,17 @@ namespace JohnTheRobot
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var john = new Humanoid(new Dancing());
+            Console.WriteLine(john.ShowSkill()); //print dancing
+
+            var alex = new Humanoid(new Cooking());
+            Console.WriteLine(alex.ShowSkill());//print cooking
+
+            var bob = new Humanoid();
+            Console.WriteLine(bob.ShowSkill());//print no skill is defined
+
         }
     }
 }
